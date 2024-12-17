5c2d76a6cbf311f8acb7d064a972507309eb5dc4
AAKASHRAJKASHYAP <96673633+AAKASHRAJKASHYAP@users.noreply.github.com>
2024-12-17 14:34:47 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+const math=require("./math");
+
+
+console.log("Hey , i am JS");
+console.log("two nos are",math.addTwoNumbers(2,4)); 
+console.log("two nos are",math.substract(2,4)); 
+
---
+++ math.js
@@ -0,0 +1,12 @@
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+function substract(a,b)
+{
+    return a-b;
+}
+
+//console.log(addTwoNumbers(3, 5));
+
+module.exports={addTwoNumbers,substract};
