1796dad87295be3903a912c862a5072cad5b34cf
AAKASHRAJKASHYAP <96673633+AAKASHRAJKASHYAP@users.noreply.github.com>
2024-12-17 15:46:14 +0530

    commit 2nd

--- file.js
+++ file.js
@@ -1,1 +1,18 @@
-const fs=require("fs")
+const fs=require("fs");
+const os=require("os");
+// sync..
+//fs.writeFileSync('./test.txt',"Hi my first text file");
+
+//Aysnc
+//fs.writeFile('./test.txt',"Hello World Async",(err)=>{});
+
+// const result=fs.readFileSync("./contact.txt","utf-8");
+// console.log(result);
+
+// fs.readFile("./contact.txt", "utf-8", (err, result) => {  // Read the file asynchronously
+//     if (err) {  // If there's an error (e.g., file not found)
+//         console.log("Error", err);  // Log the error message
+//     } else {
+//         console.log(result);  // If the file is read successfully, log its content
+//     }
+// });
